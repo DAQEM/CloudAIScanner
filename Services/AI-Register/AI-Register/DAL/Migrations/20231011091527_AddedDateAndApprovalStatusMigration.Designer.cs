58fe81a88f19da8a93670c7e8a5be7fd320c1cdb
Danny <danny.cornelissen38@gmail.com>
2023-10-11 11:18:57 +0200

    added migration to add date and approval status

--- Services/AI-Register/AI-Register/DAL/Migrations/20231011091527_AddedDateAndApprovalStatusMigration.Designer.cs
+++ Services/AI-Register/AI-Register/DAL/Migrations/20231011091527_AddedDateAndApprovalStatusMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DAL.Migrations
 {
     [DbContext(typeof(AIRegisterDBContext))]
-    partial class AIRegisterDBContextModelSnapshot : ModelSnapshot
+    [Migration("20231011091527_AddedDateAndApprovalStatusMigration")]
+    partial class AddedDateAndApprovalStatusMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
