8c2545e3a326169bcf7ba8318ed2de875f5868ba
Melanie <melanievandegraaf1@live.nl>
2023-10-25 09:36:05 +0200

    Added Service

---
+++ Services/AiExtractionService/Logic/Services/GetServiceData.cs
@@ -0,0 +1,22 @@
+﻿using Google.Api.Gax;
+
+namespace Logic.Services;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.ResourceManager.V3;
+using Google.Cloud.ServiceUsage.V1;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class GetServiceData : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get(string accessToken)
+    {
+        GoogleCredential credential = GoogleCredential.FromAccessToken(accessToken);
+        ProjectsClient = new ProjectsClientBuilder
+        { Credential = credential.}Build();
+        
+        PagedEnumerable<SearchProjectsResponse, Projects>
+    }
+}
